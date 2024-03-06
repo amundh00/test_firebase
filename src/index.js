d1fba468029f6d0e14e947a0658742ff84fa1891
Amund Halgunset <amundhalgunset@gmail.com>
2024-03-06 12:44:40 +0100

    game æ?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Hello Amund!')
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBB5l8VXiz1yPHHh67KxhuMCI-IfUt1X6o",
+    authDomain: "revedilter.firebaseapp.com",
+    projectId: "revedilter",
+    storageBucket: "revedilter.appspot.com",
+    messagingSenderId: "89877137581",
+    appId: "1:89877137581:web:2c02364e850c9c83c7de24"
+  };
+
+
+  //init firebase app
+  initializeApp(firebaseConfig);
+
+  // init services
+  const db = getFirestore();
+
+  // collection ref
+  const colRef = collection(db, 'opening_hours');
+
+  // get collection data
+  getDocs(colRef)
+    .then((snapshot) => {
+        console.log(snapshot.docs);
+    })
